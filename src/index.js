3fadd63e315767f6468d3c47b3ef87deec908165
shakhovskaya <shahnat@tut.by>
2019-03-02 17:15:15 +0300

    feat: the task is resolved

--- src/index.js
+++ src/index.js
@@ -4,4 +4,20 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+
+  let set = preferences.reduce((s, current, index, pref) => {
+    let v1 = index + 1;
+    let v2 = current;
+    let v3 = pref[v2 - 1];
+    if (v3 !== undefined && pref[v3 - 1] === v1) {
+      s.add(v1);
+      s.add(v2);
+      s.add(v3);
+    }
+    return s;
+  }, new Set());
+
+  let count = Math.floor(set.size / 3);
+
+  return count;
 };
