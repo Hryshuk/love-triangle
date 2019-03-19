95cf9af9958fe721451c970ee2228893a17e9d87
Dmitry Hryshuk <hryshuk@tut.by>
2019-03-19 12:55:05 +0300

    feat: resolved

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
 
-  let set = preferences.reduce((s, current, index, pref) => {
-    let v1 = index + 1;
-    let v2 = current;
-    let v3 = pref[v2 - 1];
+  const set = preferences.reduce((s, current, index, pref) => {
+    const v1 = index + 1;
+    const v2 = current;
+    const v3 = pref[v2 - 1];
     if (v3 !== undefined && pref[v3 - 1] === v1) {
       s.add(v1);
       s.add(v2);
@@ -17,7 +17,6 @@ module.exports = function getLoveTrianglesCount(preferences = []) {
     return s;
   }, new Set());
 
-  let count = Math.floor(set.size / 3);
-
+  const count = Math.floor(set.size / 3);
   return count;
 };
